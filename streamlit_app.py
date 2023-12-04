4975092ad928c7b1aaf934f3c91474ca9404f068
LGARRO-KPC <151617923+LGARRO-KPC@users.noreply.github.com>
2023-12-04 09:06:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ streamlit.title('Test LGA')
 streamlit.header('Menu 1')
 streamlit.text('exemple 1')
 streamlit.text('exemple 2')
+
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑 🍞Hard-Boiled Free-Range Egg')
